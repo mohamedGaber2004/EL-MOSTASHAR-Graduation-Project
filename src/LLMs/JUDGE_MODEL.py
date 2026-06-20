c61c18626cb9209afbbf80e1b8c1974a2e3a7cef
Mohamed Gaber Sayed <mg7357432@gmail.com>
2026-06-20 20:16:28 +0300

    Update JUDGE_MODEL.py

--- src/LLMs/JUDGE_MODEL.py
+++ src/LLMs/JUDGE_MODEL.py
@@ -7,21 +7,21 @@ load_dotenv()
 
 class JudgeModel(BaseModel):
     def __init__(self, model_name: str, temperature: float):
-        super().__init__(model_name, temperature) 
+        super().__init__(model_name, temperature)
 
-    self.as_open_router_llm = ChatOpenAI(
+        self.as_open_router_llm = ChatOpenAI(
             model=self.model_name,
             temperature=self.temperature,
             max_tokens=4096,
             model_kwargs={
-            "extra_body": {
-                "reasoning": {"max_tokens": 1024}
+                "extra_body": {
+                    "reasoning": {"max_tokens": 1024}
                 }
             },
             base_url=self.cfg.OPENAI_BASE_URL,
             api_key=self.cfg.OPENAI_BASE_ROUTER_API_KEY
         )
-    
+
 def get_judge_model():
     cfg = get_settings()
     model = cfg.JUDGE_MODEL
